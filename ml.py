34836d03951e9caac75e221cc09c0af10104a0d1
mknavas <mknavas19@gmail.com>
2022-08-26 17:10:45 +0530

    added 3 new ML code

--- ml.py
+++ ml.py
@@ -1,1 +1,10 @@
 print("Adding all the ML related codings")
+
+def model1():
+    print('code for Model1')
+
+def model2():
+    print('code for Model2')
+
+def model3():
+    print('code for Model3')
